994be59076005cab72940355bfbb7759e043911b
Meruzh <meruzh.kiloyan@mail.ru>
2024-05-04 12:58:05 +0400

    event bus for banking microservice

---
+++ MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
+using MicroRabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _bus;
+
+        public TransferCommandHandler(IEventBus eventBus)
+        {
+            _bus = eventBus;
+        }
+
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            // publish event
+            _bus.Publish(new TransferCreatedEvent(
+                request.From,
+                request.To,
+                request.Amount
+            ));
+
+            return Task.FromResult(true);
+        }
+    }
+}
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -3,6 +3,8 @@ using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -19,10 +21,18 @@ namespace MicroRabbit.Infra.IoC
     {
         public static void Register(IServiceCollection services)
         {
+            // bus
             services.AddMediatR(typeof(IEventHandler));
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            // commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
+
+            // services
             services.AddTransient<IAccountService, AccountService>();
             services.AddTransient<IAccountRepository, AccountRepository>();
+
+            // db
             services.AddTransient<BankingDBContext>();
         }
     }
